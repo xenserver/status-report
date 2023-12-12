3eb2e8922741d679995f7edb384c549a297b45b9
Bernhard Kaindl <43588962+bernhardkaindl@users.noreply.github.com>
2023-12-12 12:00:00 +0200

    Add test_fs_funcs.py for get_recent_logs() & readKeyValueFile()

---
+++ tests/unit/test_fs_funcs.py
@@ -0,0 +1,19 @@
+"""Regression tests for bugtool helper functions like get_recent_logs()"""
+
+
+def test_get_recent_logs(bugtool):
+    """Assert get_recent_logs() returning the most recent file(s)"""
+
+    files = ["/proc/1/stat", "/proc/self/stat"]  # self/stat is more recent.
+    # return the files, ordered by their modification time, most recent first:
+    assert bugtool.get_recent_logs(files, 9) == files
+    assert bugtool.get_recent_logs([files[1], files[0]], 9) == files
+    # When verbosity is 1, only return the most recent file:
+    assert bugtool.get_recent_logs(files, 1) == [files[1]]
+
+
+def test_read_inventory(bugtool, dom0_template):
+    """Assert readKeyValueFile() reading the a xensource inventory file"""
+
+    inventory = bugtool.readKeyValueFile(dom0_template + bugtool.XENSOURCE_INVENTORY)
+    assert inventory["PRODUCT_VERSION"] == "8.3.0"
