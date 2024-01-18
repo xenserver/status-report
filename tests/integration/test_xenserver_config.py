f8cf37456da81e693cbea6f5dd7137be92923bb8
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-01-18 21:18:50 +0100

    CP-46759: Fixup Test case on testsing the SNMP functions in bugtool.
    TODO: The actual collection and filtering of files is not tested yet!
    
    The unit test checks filtering the inputs using the individual funcs.
    
    But:
    
    With this commit, from an actual bugtool run,
    we only test the non-existing files case!
    
    Preferaby:
    
    You should add the new config files as they are used now
    to tests/integration/dom0-template, then verify that the
    contents are the filtered contents.
    
    However, without that final TODO done, we can already
    merge this and do the actual collection of the files
    in a new PR.
    
    Co-authored-by: Bengang Yuan <bengang.yuan@cloud.com>
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- tests/integration/test_xenserver_config.py
+++ tests/integration/test_xenserver_config.py
@@ -1,7 +1,7 @@
 """tests/integration/test_xenserver_config.py: Test xen-bugtool --entries=xenserver-config"""
 import os
 
-from .utils import assert_cmd, check_file, run_bugtool_entry, assert_content_from_dom0_template
+from .utils import assert_cmd, assert_file, run_bugtool_entry, assert_content_from_dom0_template
 
 
 def test_xenserver_config(output_archive_type):
@@ -11,9 +11,9 @@ def test_xenserver_config(output_archive_type):
     run_bugtool_entry(output_archive_type, entry)
 
     # Assert that the bugtool output archive of --entries=xenserver-config matches our expectations for it:
-    assert check_file("ls-lR-%opt%xensource.out").splitlines()[0] == "/opt/xensource:"
-    assert check_file("ls-lR-%etc%xensource%static-vdis.out") == ""
-    assert check_file("static-vdis-list.out") == "list"
+    assert_file("ls-lR-%opt%xensource.out", "/opt/xensource:", only_first_line=True)
+    assert_file("ls-lR-%etc%xensource%static-vdis.out", "")
+    assert_file("static-vdis-list.out", "list")
 
     # Assert the contents of the extracted etc/systemd.tar
     os.chdir("..")
@@ -24,6 +24,22 @@ def test_xenserver_config(output_archive_type):
     assert_content_from_dom0_template("etc/systemd")
     assert_content_from_dom0_template("etc/xensource-inventory")
 
-    assert check_file("snmp_xs_conf.out") != ""
-    assert check_file("snmpd_xs_conf.out") != ""
-    assert check_file("snmpd_conf.out") != ""
+    # Sample SNMP config files are currently not in the dom0_template!
+    # Reading them records the error message in the file content, do we want this?
+    # I think the "Failed to filter" is redundant in it.
+    # Maybe decide on a standardized error for missing files in bugtool?
+
+    # TODO: To be clarified or fixed as part of CP-46759 or a follow-up!
+
+    for input_file in [
+        "/etc/snmp/snmp.xs.conf",
+        "/etc/snmp/snmpd.xs.conf",
+        "/var/lib/net-snmp/snmpd.conf",
+    ]:
+        assert_file(
+            input_file.split("/")[-1].replace(".", "_") + ".out",
+            # That's a very long error message an the 1st two parts are redundant:
+            "Failed to filter %s "
+            "[Errno 2] "
+            "No such file or directory: '%s'" % (input_file, input_file),
+        )
--- tests/unit/test_snmp.py
+++ tests/unit/test_snmp.py
@@ -1,65 +1,44 @@
-"""Regression tests for bugtool functions like filter_snmp*_conf()"""
-
-import sys
+"""Unit tests for the bugtool SNMP filter functions: filter_snmp.*_conf()"""
 
 
-if sys.version_info.major == 3:
-    builtin_module_name = "builtins"
-else:
-    builtin_module_name = "__builtin__"
+def test_filter_snmp_xs_conf(bugtool, builtins, mocker):
+    """Assert that filter_snmp_xs_conf() replaces sensitive strings"""
 
-
-def test_filter_snmp_xs_conf(bugtool, mocker):
-    """Assert filter_snmp_xs_conf() replaces sensitive strings"""
-    snmp_xs_conf_init_str = """
-        "community": "INITIAL_STR",
-        "authentication_key": "INITIAL_STR",
-        "privacy_key": "INITIAL_STR",
+    snmp_xs_conf_input = """
+        "community": "SECRET",
+        "authentication_key": "SECRET",
+        "privacy_key": "SECRET",
     """
-    mocker.patch(
-        "{}.open".format(builtin_module_name),
-        new=mocker.mock_open(read_data=snmp_xs_conf_init_str),
-        create=True,
-    )
-
-    snmp_xs_conf_out_str = """
+    snmp_xs_conf_output = """
         "community": "REMOVED",
         "authentication_key": "REMOVED",
         "privacy_key": "REMOVED",
     """
-    assert bugtool.filter_snmp_xs_conf("_") == snmp_xs_conf_out_str
+    mocker.patch(builtins + ".open", mocker.mock_open(read_data=snmp_xs_conf_input))
+    assert bugtool.filter_snmp_xs_conf("_") == snmp_xs_conf_output
 
 
-def test_filter_snmpd_xs_conf(bugtool, mocker):
-    """Assert filter_snmpd_xs_conf() replaces sensitive strings"""
-    snmpd_xs_conf_init_str = "com2sec notConfigUser default INITIAL_STR"
-    mocker.patch(
-        "{}.open".format(builtin_module_name),
-        new=mocker.mock_open(read_data=snmpd_xs_conf_init_str),
-        create=True,
-    )
+def test_filter_snmpd_xs_conf(bugtool, builtins, mocker):
+    """Assert that filter_snmpd_xs_conf() replaces sensitive strings"""
 
-    snmpd_xs_conf_out_str = "com2sec notConfigUser default REMOVED"
-    assert bugtool.filter_snmpd_xs_conf("_") == snmpd_xs_conf_out_str
+    snmpd_xs_conf_input = "com2sec notConfigUser default SECRET"
+    snmpd_xs_conf_output = "com2sec notConfigUser default REMOVED"
+    mocker.patch(builtins + ".open", mocker.mock_open(read_data=snmpd_xs_conf_input))
+    assert bugtool.filter_snmpd_xs_conf("_") == snmpd_xs_conf_output
 
 
-def test_filter_snmpd_conf(bugtool, mocker):
-    """Assert filter_snmpd_conf() replaces sensitive strings"""
-    snmpd_conf_init_str = (
+def test_filter_snmpd_conf(bugtool, builtins, mocker):
+    """Assert that filter_snmpd_conf() replaces sensitive strings"""
+
+    snmpd_conf_input = (
         "usmUser 1 3 0x80001f8880f369b576d8b2a46500000000 0x7872746d69612d30372d3035 "
         + "0x7872746d69612d30372d3035 NULL .1.3.6.1.6.3.10.1.1.3 "
-        + "INITIAL_STR "
+        + "SECRET "
         + ".1.3.6.1.6.3.10.1.2.2 "
-        + "INITIAL_STR "
+        + "SECRET "
         + "0x"
     )
-    mocker.patch(
-        "{}.open".format(builtin_module_name),
-        new=mocker.mock_open(read_data=snmpd_conf_init_str),
-        create=True,
-    )
-
-    snmpd_conf_out_str = (
+    snmpd_conf_output = (
         "usmUser 1 3 0x80001f8880f369b576d8b2a46500000000 0x7872746d69612d30372d3035 "
         + "0x7872746d69612d30372d3035 NULL .1.3.6.1.6.3.10.1.1.3 "
         + "REMOVED "
@@ -67,4 +46,5 @@ def test_filter_snmpd_conf(bugtool, mocker):
         + "REMOVED "
         + "0x"
     )
-    assert bugtool.filter_snmpd_conf("_") == snmpd_conf_out_str
+    mocker.patch(builtins + ".open", mocker.mock_open(read_data=snmpd_conf_input))
+    assert bugtool.filter_snmpd_conf("_") == snmpd_conf_output
