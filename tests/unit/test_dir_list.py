3bbe7accfd7e1b74dfc243944e52c4dfc20becb4
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-02-07 21:46:23 +0100

    CA-388587 Add tests for filter_xapi_clusterd_db()
    Includes:
    conftest.py: Add tmpdir fixture: run test in tmpdir and check leftovers
    
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- tests/unit/test_dir_list.py
+++ tests/unit/test_dir_list.py
@@ -1,24 +1,24 @@
 """tests/unit/test_dir_list.py: Unit-Test bugtool.dir_list(cap, path_list)"""
 
 
-def test_dir_list(bugtool):
+def test_dir_list(bugtool_log):
     """Test the bugtool function dir_list(cap, path_list) to perform as expected"""
 
     # Arguments for the positive test:
-    cap = bugtool.CAP_XENSERVER_CONFIG
+    cap = bugtool_log.CAP_XENSERVER_CONFIG
     path_list = [__file__.replace(".", "*")]
 
     # Assert that mis-matching cap causes no output in bugtool.data:
-    bugtool.entries = [bugtool.CAP_DISK_INFO, cap, bugtool.CAP_PAM]
-    bugtool.dir_list(cap=bugtool.CAP_XENSERVER_LOGS, path_list=path_list)
-    assert bugtool.data == {}
+    bugtool_log.entries = [bugtool_log.CAP_DISK_INFO, cap, bugtool_log.CAP_PAM]
+    bugtool_log.dir_list(cap=bugtool_log.CAP_XENSERVER_LOGS, path_list=path_list)
+    assert bugtool_log.data == {}
 
     # Assert that mis-matching path_list results in no bugtool.data:
-    bugtool.dir_list(cap=cap, path_list=[__file__ + "*notexist*"])
-    assert bugtool.data == {}
+    bugtool_log.dir_list(cap=cap, path_list=[__file__ + "*notexist*"])
+    assert bugtool_log.data == {}
 
     # Assert matching cap and path_list produces expected_data
-    bugtool.dir_list(cap=cap, path_list=path_list)
+    bugtool_log.dir_list(cap=cap, path_list=path_list)
 
     expected_data = {
         "ls -l "
@@ -32,4 +32,4 @@ def test_dir_list(bugtool):
             ],
         }
     }
-    assert bugtool.data == expected_data
+    assert bugtool_log.data == expected_data
